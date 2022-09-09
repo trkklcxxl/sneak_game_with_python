8bdbf9b62cc0701fc2b96f027f14cceb87d417fe
T.kilicAcer <trkklc28600@gmail.com>
2022-09-09 16:34:38 +0300

    first push

---
+++ sneak_game_console.py
@@ -0,0 +1,56 @@
+import os
+import numpy as np
+import time
+class Sneak:
+    def __init__(self,size):
+        self.size=size
+        
+
+class GameScreen:
+    def __init__(self,sneak):
+        self.sneak=sneak
+    def CreateScreen(self):
+        screen=np.zeros(10,10)
+
+
+
+        
+class GameEngiene:
+    def __init__(self,sneak,gameScreen):
+        self.sneak=sneak
+        self.gameScrean=gameScrean
+
+
+
+        
+def cls():
+    os.system('cls')
+    
+screen=GameScreen("deneme")
+print(screen.sneak)
+
+arr=[[5]]
+arr[0][2]='a'
+print(arr)
+
+"""
+time.sleep(2)
+cls()
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
