c2e63b7d0302af0179dc0cfbc553302c79e90269
T.kilicAcer <trkklc28600@gmail.com>
2022-09-12 14:17:29 +0300

    ekran çizildi

--- sneak_game_console.py
+++ sneak_game_console.py
@@ -1,6 +1,13 @@
 import os
 import numpy as np
 import time
+
+# https://github.com/clear-code-projects/powershell-snake/blob/main/snake.py 
+
+#zero -> space
+#one -> barrier
+#two -> sneak´s body
+
 class Sneak:
     def __init__(self,size):
         self.size=size
@@ -9,34 +16,58 @@ class Sneak:
 class GameScreen:
     def __init__(self,sneak):
         self.sneak=sneak
-    def CreateScreen(self):
-        screen=np.zeros(10,10)
-        print(screen)
-        
-    
-
+    def CreateBoard(self):
+        board=np.zeros((20,40))
+        for i in range(20):
+            for j in range(40):
+                if(i==0):
+                    board[i][j]=1
+                if(i==19):
+                    board[i][j]=1
+                if(j==0):
+                    board[i][j]=1
+                if(j==39):
+                    board[i][j]=1
+                if(i==9 and j==19):
+                    board[i][j]=2
+        return board
+    def PrintBoard(self,board):
+        for i in range(20):
+            for j in range(40):
+                if(board[i][j]==1):
+                    print("*",end="")
+                elif(board[i][j]==0):
+                    print(" ",end="")
+                elif(board[i][j]==2):
+                    print("x",end="")
+            print("\n",end="")
+                    
 
 
         
 class GameEngiene:
-    def __init__(self,sneak,gameScreen):
-        self.sneak=sneak
-        self.gameScrean=gameScrean
+    def __init__(self,board):
+        self.board=board
 
-
-
+    def Move():
         
+            
 def cls():
     os.system('cls')
+
+
+
+
     
 screen=GameScreen("deneme")
-
-
-
-"""
-time.sleep(2)
+board=screen.CreateBoard()
+engiene=GameEngiene(board)
 cls()
-"""
+screen.PrintBoard(board)
+time.sleep(0.5)
+cls()
+time.sleep(0.5)
+screen.PrintBoard(board)
 
 
 
